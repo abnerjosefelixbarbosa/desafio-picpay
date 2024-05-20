2b315e9f507cb0549dc3b6405024d57c2348ebb7
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2024-05-20 19:24:28 -0300

    commited by Abner

---
+++ backend-java/src/main/java/com/org/backendjava/model/entity/Transfer.java
@@ -0,0 +1,42 @@
+package com.org.backendjava.model.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "transfer_tb")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transfer implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "date_time")
+	private LocalDateTime dateTime;
+	@Column(name = "value_transfer")
+	private BigDecimal value;
+	@ManyToOne
+	@JoinColumn(name = "payer_id")
+	private User payer;
+	@ManyToOne
+	@JoinColumn(name = "payee_id")
+	private User payee;
+}
