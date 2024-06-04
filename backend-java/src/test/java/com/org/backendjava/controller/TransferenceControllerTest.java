f799d9c18e95884d4c1caea10dad1d5bd02250b3
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2024-06-04 15:56:33 -0300

    commited by Abner

--- backend-java/src/test/java/com/org/backendjava/controller/TransferenceControllerTest.java
+++ backend-java/src/test/java/com/org/backendjava/controller/TransferenceControllerTest.java
@@ -5,6 +5,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -16,8 +17,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.org.backendjava.interfaces.ITransferenceService;
 import com.org.backendjava.model.dto.TransferValueDTO;
+import com.org.backendjava.model.entity.Transference;
+import com.org.backendjava.model.entity.User;
+import com.org.backendjava.repository.TransferenceRepository;
+import com.org.backendjava.repository.UserRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -27,7 +31,9 @@ public class TransferenceControllerTest {
 	@Autowired
 	private ObjectMapper objectMapper;
 	@Autowired
-	private ITransferenceService transferenceService;
+	private UserRepository userRepository;
+	@Autowired
+	private TransferenceRepository transferenceRepository;
 
 	@Test
 	@Disabled
@@ -44,9 +50,12 @@ public class TransferenceControllerTest {
 	@Test
 	@Disabled
 	public void shouldListTransferencesByPayerAndReturnStatus200() throws Exception {
-        TransferValueDTO dto = new TransferValueDTO(BigDecimal.valueOf(100), Long.valueOf(1), Long.valueOf(2));
-        transferenceService.transferValue(dto);
-		
+		User payer = userRepository.findById(Long.valueOf(1)).orElse(null);
+		User payee = userRepository.findById(Long.valueOf(2)).orElse(null);
+		Transference transference = new Transference(null, LocalDateTime.now(), BigDecimal.valueOf(100), payer, payee);
+
+		transferenceRepository.save(transference);
+
 		mockMvc.perform(get("/api/transferences/list-transference-by-player?payer=1&&number=0&&size=5"))
 				.andExpect(MockMvcResultMatchers.status().isOk()).andDo(print());
 	}
